64f4eccf042e1b5820d002f27b0505bf956ae1c0
Mahesh S R <mahepj94@gmail.com>
2015-02-14 17:10:30 +0530

    libcurl

--- proxy.php
+++ proxy.php
@@ -1,10 +1,14 @@
 <?php
-$URL = $_GET["url"];
-$file = fopen("$URL", "r");
 
-while(!feof($file)) {
-	$line = fgets($file);
-	echo $line;
-}
+	$cSession = curl_init(); 
 
+	curl_setopt($cSession,CURLOPT_URL,"http://www.google.com/search?q=youtube");
+	curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($cSession,CURLOPT_HEADER, false); 
+
+	$result=curl_exec($cSession);
+
+	curl_close($cSession);
+
+	echo $result;
 ?>
